10b5caae17597614bb97e42ee3f968a08d37c50a
Charan-Valign <charan@v-align.in>
2026-06-14 11:04:50 +0530

    fix:Handle the call function

--- app.py
+++ app.py
@@ -12,21 +12,24 @@ if "messages" not in st.session_state:
 user_input = st.text_input("Ask your question:")
 
 if user_input:
-    result = get_best_match(user_input, faqs)
 
-    if result["score"] < 50:
-        answer = "Sorry, I couldn't find a good match."
+    if not user_input.strip():
+        st.warning("Please enter a valid question.")
+
+    elif len(user_input) > 150:
+        st.warning("Question too long. Please shorten it.")
+
     else:
-        answer = result["answer"]
+        result = get_best_match(user_input, faqs)
 
-    st.session_state["messages"].append(("You", user_input))
-    st.session_state["messages"].append(("Bot", answer))
+        if result["score"] < 50:
+            answer = "Sorry, I couldn't find a good match."
+        else:
+            answer = result["answer"]
+
+
+        st.session_state["messages"].append(("You", user_input))
+        st.session_state["messages"].append(("Bot", answer))
 
 for role, msg in st.session_state["messages"]:
     st.write(f"**{role}:** {msg}")
-
-if user_input:
-    if not user_input.strip():
-        st.warning("Please enter a valid question.")
-    elif len(user_input) > 150:
-        st.warning("Question too long. Please shorten it.")
